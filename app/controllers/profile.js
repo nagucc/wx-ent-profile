5bb8524a7e756f3cc4738344742aab196be341bb
Ynu AppEngine <yae@ynu.edu.cn>
2015-09-10 10:57:44 +0800

    update

--- app/controllers/profile.js
+++ app/controllers/profile.js
@@ -85,13 +85,11 @@ var EventHandlers = {
         var wxapi = require('../models/wxent-api-redis')(wxcfg.corpId, wxcfg.secret, wxcfg.agentId, config.redis.host, config.redis.port);
         wxapi.getUser(msg.FromUserName, function (err, user) {
             if(err || user.errcode !== 0){
-                res.reply('发生错误，请将错误代码发给管理员：' + err);
+                res.reply('发生错误，请将错误代码发给管理员2：' + err);
             } else {
                 res.reply('您当前登记的手机号是：' + user.mobile);
             }
-        });
-        
-        
+        });  
 	},
 
     'base_email': function (msg, req, res, next) {
