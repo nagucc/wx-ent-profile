6c5908ec24986857bf94c9b9ea2574655dcfe472
Ynu AppEngine <yae@ynu.edu.cn>
2015-09-10 11:28:27 +0800

    update

--- app/models/access-token-redis.js
+++ app/models/access-token-redis.js
@@ -37,7 +37,7 @@ At.prototype.getToken = function (callback) {
     self.client.get(self.appId +'.expire', function(err, date){
         if(err) callback(err);
         else if(!date) callback(null, null);            
-        else if(moment().isBefore(date)) {                                // 还在有效期内
+        else if(moment().isBefore(moment(date)) {                                // 还在有效期内
             self.client.get(self.appId + '.token', function(err, token){
                 if(err || !token) callback('error');
                 else callback(err, token);
