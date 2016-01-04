59db65d26e35b750ca091ce58b0a95be344c8fb6
liudonghua123 <liudonghua123@gmail.com>
2016-01-04 11:43:53 +0800

    fix redis environment issues

--- config/config.js
+++ config/config.js
@@ -24,7 +24,7 @@ var config = {
     },
     redis: {
       host: process.env.REDIS_HOST || 'redis',
-      port: process.env.REDIS_PORT || 6379
+      port: process.env.REDIS_PORT_MANUAL || 6379
     }
   }
 };
